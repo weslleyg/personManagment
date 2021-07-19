813d07f69365b2360b1d1d6909bcb0592ae7ccab
weslleyg <wesley.dipaulatorres@hotmail.com>
2021-07-19 19:26:48 -0300

    server on port 8081

--- src/main/java/one/digitalinnovation/personapi/entities/Person.java
+++ src/main/java/one/digitalinnovation/personapi/entities/Person.java
@@ -3,10 +3,14 @@ package one.digitalinnovation.personapi.entities;
 import java.time.LocalDate;
 import java.util.List;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -24,11 +28,17 @@ public class Person {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
+  @Column(nullable = false)
   private String firstName;
 
+  @Column(nullable = false)
   private String lastName;
+
+  @Column(nullable = false, unique = true)
+  private String cpf;
 
   private LocalDate birthDate;
 
+  @OneToMany(fetch = FetchType.LAZY, cascade = { CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REMOVE })
   private List<Phone> phones;
 }
